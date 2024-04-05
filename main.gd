2a0a6966103c950e7528791c8be249c17986435d
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-05 20:04:21 +0100

    #9 Applied a little bit of smoothing when moving the window this can appear a little delayed

--- main.gd
+++ main.gd
@@ -4,9 +4,11 @@ extends Control
 
 @export var min_window_size := Vector2i(128, 128)
 @export var max_window_size := Vector2i(512, 512)
+@export var move_window_smoothing_factor := .25
 
 var dragging_window: bool
 var drag_start_pos: Vector2
+var smoothed_mouse_pos: Vector2
 
 @onready var window := get_window()
 
@@ -18,7 +20,8 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if dragging_window:
-		window.position += Vector2i(get_local_mouse_position() - drag_start_pos)
+		smoothed_mouse_pos = smoothed_mouse_pos.lerp(get_local_mouse_position(), move_window_smoothing_factor)
+		window.position += Vector2i(smoothed_mouse_pos - drag_start_pos)
 
 
 func close_window() -> void:
@@ -29,4 +32,6 @@ func _on_hbc_chrome_gui_input(event: InputEvent) -> void:
 	var mouse_button_event := event as InputEventMouseButton
 	if mouse_button_event and mouse_button_event.button_index == 1: # Left mouse click
 		dragging_window = !dragging_window
-		drag_start_pos = get_local_mouse_position()
+		var m_pos := get_local_mouse_position()
+		drag_start_pos = m_pos
+		smoothed_mouse_pos = m_pos
