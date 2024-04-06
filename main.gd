ac39749788463e47c6049b2078aa60d8b3d7d1b9
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-06 17:25:47 +0100

    Reset functionality

--- main.gd
+++ main.gd
@@ -1,7 +1,7 @@
 class_name Main
 extends Control
 
-
+@export_category("Window")
 @export var min_window_size := Vector2i(128, 128)
 @export var max_window_size := Vector2i(512, 512)
 @export var move_window_smoothing_factor := .99
--- scenes/stopwatch.gd
+++ scenes/stopwatch.gd
@@ -18,15 +18,25 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	elapsed_time += delta
+	_set_time()
 
+
+func set_state(state: bool) -> void:
+	set_process(state)
+	modulate = ticking_colour if state else paused_colour
+
+
+func reset() -> void:
+	set_process(false)
+	modulate = paused_colour
+	elapsed_time = 0.0
+	_set_time()
+
+
+func _set_time() -> void:
 	_l_time.text = _time_text_template % [
 			(elapsed_time / 3600.0),
 			(fmod(elapsed_time, 3600.0) / 60.0),
 			(fmod(elapsed_time, 60.0)),
 			(fmod(elapsed_time, 1) * 100.0)
 	]
-
-
-func set_state(state: bool) -> void:
-	set_process(state)
-	modulate = ticking_colour if state else paused_colour
