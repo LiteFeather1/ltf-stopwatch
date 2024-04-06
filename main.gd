16d547de97f3ee868de73e9b3cfb8d834704a16e
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-06 14:01:30 +0100

    Smother window dragging

--- main.gd
+++ main.gd
@@ -4,7 +4,7 @@ extends Control
 
 @export var min_window_size := Vector2i(128, 128)
 @export var max_window_size := Vector2i(512, 512)
-@export var move_window_smoothing_factor := .25
+@export var move_window_smoothing_factor := .99
 
 var dragging_window: bool
 var drag_start_pos: Vector2
@@ -20,7 +20,7 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	if dragging_window:
-		smoothed_mouse_pos = smoothed_mouse_pos.lerp(get_local_mouse_position(), move_window_smoothing_factor)
+		smoothed_mouse_pos = smoothed_mouse_pos.lerp(window.get_mouse_position(), move_window_smoothing_factor)
 		window.position += Vector2i(smoothed_mouse_pos - drag_start_pos)
 
 
@@ -32,6 +32,6 @@ func _on_hbc_chrome_gui_input(event: InputEvent) -> void:
 	var mouse_button_event := event as InputEventMouseButton
 	if mouse_button_event and mouse_button_event.button_index == 1: # Left mouse click
 		dragging_window = !dragging_window
-		var m_pos := get_local_mouse_position()
+		var m_pos := window.get_mouse_position()
 		drag_start_pos = m_pos
 		smoothed_mouse_pos = m_pos
