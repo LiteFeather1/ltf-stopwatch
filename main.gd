5358b748035e86ee7e2113b4c255594d1b03fd3b
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-07 10:43:46 +0100

    Moved all the chrome logic to it's on scene and script

--- main.gd
+++ main.gd
@@ -11,42 +11,14 @@ extends Control
 @export var b_start: Button
 @export var b_reset: Button
 
-var dragging_window: bool
-var drag_start_pos: Vector2
-var smoothed_mouse_pos: Vector2
-
-@onready var window := get_window()
-
 
 func _ready() -> void:
+	var window := get_window()
 	window.min_size = min_window_size
 	window.max_size = max_window_size
 
 	b_start.toggled.connect(_start_pressed)
 	b_reset.pressed.connect(_reset_pressed)
-
-
-func _process(_delta: float) -> void:
-	if dragging_window:
-		smoothed_mouse_pos = smoothed_mouse_pos.lerp(window.get_mouse_position(), move_window_smoothing_factor)
-		window.position += Vector2i(smoothed_mouse_pos - drag_start_pos)
-
-
-func _on_hbc_chrome_gui_input(event: InputEvent) -> void:
-	var mb_event := event as InputEventMouseButton
-	if mb_event and mb_event.button_index == 1: # Left mouse click
-		dragging_window = not dragging_window
-		var m_pos := window.get_mouse_position()
-		drag_start_pos = m_pos
-		smoothed_mouse_pos = m_pos
-
-
-func _close_window() -> void:
-	get_tree().quit()
-
-
-func _minimize_window() -> void:
-	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_MINIMIZED)
 
 
 func _stopwatch_started() -> void:
@@ -55,6 +27,7 @@ func _stopwatch_started() -> void:
 
 func _start_pressed(state: bool) -> void:
 	b_start.text = "P" if state else "C"
+
 
 func _reset_pressed() -> void:
 	b_start.button_pressed = false
---
+++ scenes/chrome.gd
@@ -0,0 +1,37 @@
+class_name Chrome extends HBoxContainer
+
+
+@export var move_window_smoothing_factor := .99
+
+var dragging: bool
+var start_drag_pos: Vector2
+var smoothed_mouse_pos: Vector2
+
+
+@onready var window: Window = get_window()
+
+
+func _process(_delta: float) -> void:
+	if not dragging:
+		return
+	
+	smoothed_mouse_pos = smoothed_mouse_pos.lerp(
+			window.get_mouse_position(), move_window_smoothing_factor)
+	window.position += Vector2i(smoothed_mouse_pos - start_drag_pos)
+
+
+func _on_chrome_gui_input(event: InputEvent) -> void:
+	var mb_event := event as InputEventMouseButton
+	if mb_event and mb_event.button_index == 1: # Left mouse click
+		dragging = not dragging
+		var m_pos := window.get_mouse_position()
+		start_drag_pos = m_pos
+		smoothed_mouse_pos = m_pos
+
+
+func _close_window() -> void:
+	get_tree().quit()
+
+
+func _minimise_window() -> void:
+	window.mode = Window.MODE_MINIMIZED
