fe8db6221012e0d32030580db42c250d614fc81d
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-08 19:30:29 +0100

    Fixed a bug where the size of the pop up was not being set properly

--- scenes/pop_up.gd
+++ scenes/pop_up.gd
@@ -2,17 +2,27 @@ class_name PopUp extends ColorRect
 
 
 @export var l_text: Label
-@export var label_padding := Vector2(10.0, 5.0)
+@export var label_padding := Vector2(12.0, 8.0)
 @export var dent: Control
 @export var _animation_duration := .25
 
+var font: Font
+var font_size : int
+
 var _tween: Tween
+
+
+func _ready() -> void:
+	font = l_text.get_theme_font("font")
+	font_size = l_text.get_theme_font_size("font_size")
+
 
 func pop_up(c: Control, text: String) -> void:
 	l_text.text = text
+	var s: Vector2 = font.get_string_size(text, HORIZONTAL_ALIGNMENT_CENTER, -1, font_size)
 
-	size = l_text.size + label_padding
-	pivot_offset = size * .5
+	size = s + label_padding
+	pivot_offset = s * .5
 
 	global_position = Vector2(
 		c.global_position.x + (c.size.x - size.x) * .5,
