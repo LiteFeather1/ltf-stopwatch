9cb6154536ec835f2be8dedbf6ca44ba46e4e496
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-08 17:29:38 +0100

    Unpopup method. Fixed a bug in which tweens could overlap each other

--- scenes/button_pop_up.gd
+++ scenes/button_pop_up.gd
@@ -5,10 +5,9 @@ class_name ButtonPopUp extends ColorRect
 @export var label_padding := Vector2(10.0, 5.0)
 @export var dent: Control
 
+var tween: Tween
 
 func pop_up(c: Control, text: String) -> void:
-	visible = true
-
 	l_text.text = text
 
 	size = l_text.size + label_padding
@@ -18,8 +17,22 @@ func pop_up(c: Control, text: String) -> void:
 		c.global_position.x + (c.size.x - size.x) * .5,
 		c.global_position.y + c.size.y + dent.size.y + label_padding.y)
 	
-	var t := create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_ELASTIC)
+	visible = true
 	scale = Vector2(.75, .75)
-	t.tween_property(self, "scale", Vector2(1.0, 1.0), .5)
+	if tween:
+		tween.kill()
+	
+	
+	tween = create_tween().set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), .5)
 
 
+func unpop() -> void:
+	if tween:
+		tween.kill()
+	
+	tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_ELASTIC)
+	tween.tween_property(self, "scale", Vector2(.5, .5), .5)
+	await tween.finished
+
+	visible = false
