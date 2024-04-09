2a1c4ccaa7ca81c50903e7c4f33990dde8b9107b
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-09 12:40:27 +0100

    Close #25

--- main.gd
+++ main.gd
@@ -2,12 +2,11 @@ class_name Main extends Control
 
 
 @export_category("Window")
-@export var min_window_size := Vector2i(192, 192)
-@export var max_window_size := Vector2i(512, 512)
+@export var _min_window_size := Vector2i(192, 192)
+@export var _max_window_size := Vector2i(512, 512)
 
 
 func _ready() -> void:
 	var window := get_window()
-	window.min_size = min_window_size
-	window.max_size = max_window_size
-	
+	window.min_size = _min_window_size
+	window.max_size = _max_window_size
