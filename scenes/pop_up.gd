b9752c3a2660643dd1f4840ef9a318d597827c0a
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-09 11:35:54 +0100

    #25 pop_up is now inline with new guidelines

--- scenes/pop_up.gd
+++ scenes/pop_up.gd
@@ -1,14 +1,14 @@
 class_name PopUp extends ColorRect
 
 
-@export var l_text: Label
-@export var label_padding := Vector2(12.0, 8.0)
-@export var dent: Control
+@export var _l_text: Label
+@export var _label_padding := Vector2(12.0, 8.0)
+@export var _dent: Control
 @export var _animation_duration := .15
 @export var _delay_to_appear: Timer
 
-var font: Font
-var font_size : int
+var _font: Font
+var _font_size : int
 
 var _tween: Tween
 
@@ -16,8 +16,8 @@ var _tween: Tween
 func _ready() -> void:
 	_delay_to_appear.timeout.connect(_pop_up_animation)
 
-	font = l_text.get_theme_font("font")
-	font_size = l_text.get_theme_font_size("font_size")
+	_font = _l_text.get_theme_font("_font")
+	_font_size = _l_text.get_theme_font_size("_font_size")
 
 
 func pop_up(c: Control, text: String) -> void:
@@ -25,15 +25,15 @@ func pop_up(c: Control, text: String) -> void:
 		_tween.kill()
 		visible = false
 	
-	l_text.text = text
-	var s: Vector2 = font.get_string_size(text, HORIZONTAL_ALIGNMENT_CENTER, -1, font_size)
+	_l_text.text = text
+	var s: Vector2 = _font.get_string_size(text, HORIZONTAL_ALIGNMENT_CENTER, -1, _font_size)
 
-	size = s + label_padding
+	size = s + _label_padding
 	pivot_offset = s * .5
 
 	global_position = Vector2(
 		c.global_position.x + (c.size.x - size.x) * .5,
-		c.global_position.y + c.size.y + dent.size.y + label_padding.y * .5)
+		c.global_position.y + c.size.y + _dent.size.y + _label_padding.y * .5)
 	
 	_delay_to_appear.paused = false
 	_delay_to_appear.start(_delay_to_appear.wait_time)
