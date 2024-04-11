ae5715444f9d590a1602b205b0aaa9bd442a1e5c
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-11 15:33:46 +0100

    Inverted move logic to match vec2 x first then y

--- scenes/resize_handler.gd
+++ scenes/resize_handler.gd
@@ -28,17 +28,7 @@ func _process(_delta: float) -> void:
 
 	var mouse_delta: Vector2i = get_global_mouse_position() - _mouse_start_pos
 	# Check if it's vertical
-	if mouse_default_cursor_shape == CURSOR_VSIZE:
-		if global_position.y < _window.size.y / 2.0:
-			if (_window.size.y == _window.max_size.y and mouse_delta.y <= 0.0)\
-					or (_window.size.y == _window.min_size.y and mouse_delta.y >= 0.0):
-				return
-			
-			_window.position.y += mouse_delta.y - _distance_to_edge.y
-			_window.size.y = _window_start_size.y + _window_start_pos.y - _window.position.y
-		else:
-			_window.size.y = mouse_delta.y + _distance_to_edge.y
-	else:
+	if mouse_default_cursor_shape == CURSOR_HSIZE:
 		if global_position.x < _window.size.x / 2.0:
 			if (_window.size.x == _window.max_size.x and mouse_delta.x <= 0.0)\
 					or (_window.size.x == _window.min_size.x and mouse_delta.x >= 0.0):
@@ -48,6 +38,17 @@ func _process(_delta: float) -> void:
 			_window.size.x = _window_start_size.x + _window_start_pos.x - _window.position.x
 		else:
 			_window.size.x = mouse_delta.x + _distance_to_edge.x
+	else:
+		if global_position.y < _window.size.y / 2.0:
+			if (_window.size.y == _window.max_size.y and mouse_delta.y <= 0.0)\
+					or (_window.size.y == _window.min_size.y and mouse_delta.y >= 0.0):
+				return
+			
+			_window.position.y += mouse_delta.y - _distance_to_edge.y
+			_window.size.y = _window_start_size.y + _window_start_pos.y - _window.position.y
+		else:
+			_window.size.y = mouse_delta.y + _distance_to_edge.y
+
 
 
 func _on_gui_input(event: InputEvent) -> void:
