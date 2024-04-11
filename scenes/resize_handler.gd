1cb7a0ee7bc7ae2da03375355683f1b991b1d603
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-11 13:00:21 +0100

    Fixed a bug when resizing with left, right handler would disappear

--- scenes/resize_handler.gd
+++ scenes/resize_handler.gd
@@ -33,12 +33,15 @@ func _process(_delta: float) -> void:
 		_window.size.y = mouse_pos.y + _distance_to_edge - _mouse_offset.y
 		position.y = _window.size.y - _distance_to_edge
 	else:
+		if _window.size.x == _window.max_size.x:
+			pass
+		
 		if _left:
 			_window.position.x += mouse_pos.x - _mouse_offset.x - _distance_to_edge
 			_window.size.x = _window_size.x + _window_position.x - _window.position.x
 		else:
 			_window.size.x = mouse_pos.x + _distance_to_edge - _mouse_offset.x
-			position.x = _window.size.x - _distance_to_edge
+			# position.x = _window.size.x - _distance_to_edge
 
 
 func _on_gui_input(event: InputEvent) -> void:
