7133dbd59ecfec0e4b9176febb8f76b4d36a54e8
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-11 14:37:49 +0100

    Better namings

--- scenes/resize_handler.gd
+++ scenes/resize_handler.gd
@@ -5,9 +5,10 @@ var _move_window: bool = false
 
 var _resizing: bool
 var _distance_to_edge: int
-var _mouse_offset: Vector2i
-var _window_position: Vector2i
-var _window_size: Vector2i
+
+var _mouse_start_pos: Vector2
+var _window_start_pos: Vector2i
+var _window_start_size: Vector2i
 
 @onready var _window: Window = get_window()
 
@@ -34,37 +35,35 @@ func _process(_delta: float) -> void:
 	if not _resizing:
 		return
 
-	var mouse_pos: Vector2i = get_global_mouse_position()
+	var mouse_delta: Vector2i = get_global_mouse_position() - _mouse_start_pos
 	# Check if it's vertical
 	if mouse_default_cursor_shape == CURSOR_VSIZE:
 		if _move_window:
-			var delta := mouse_pos.y - _mouse_offset.y - _distance_to_edge
-			if (_window.size.y == _window.max_size.y and delta <= 0.0)\
-					or (_window.size.y == _window.min_size.y and delta >= 0.0):
+			if (_window.size.y == _window.max_size.y and mouse_delta.y <= 0.0)\
+					or (_window.size.y == _window.min_size.y and mouse_delta.y >= 0.0):
 				return
 			
-			_window.position.y += delta
-			_window.size.y = _window_size.y + _window_position.y - _window.position.y
+			_window.position.y += mouse_delta.y - _distance_to_edge
+			_window.size.y = _window_start_size.y + _window_start_pos.y - _window.position.y
 		else:
-			_window.size.y = mouse_pos.y + _distance_to_edge - _mouse_offset.y
+			_window.size.y = mouse_delta.y + _distance_to_edge
 	else:
 		if _move_window:
-			var delta := mouse_pos.x - _mouse_offset.x - _distance_to_edge
-			if (_window.size.x == _window.max_size.x and delta <= 0.0)\
-					or (_window.size.x == _window.min_size.x and delta >= 0.0):
+			if (_window.size.x == _window.max_size.x and mouse_delta.x <= 0.0)\
+					or (_window.size.x == _window.min_size.x and mouse_delta.x >= 0.0):
 				return
 			
-			_window.position.x += delta
-			_window.size.x = _window_size.x + _window_position.x - _window.position.x
+			_window.position.x += mouse_delta.x - _distance_to_edge
+			_window.size.x = _window_start_size.x + _window_start_pos.x - _window.position.x
 		else:
-			_window.size.x = mouse_pos.x + _distance_to_edge - _mouse_offset.x
+			_window.size.x = mouse_delta.x + _distance_to_edge
 
 
 func _on_gui_input(event: InputEvent) -> void:
 	var mb_event := event as InputEventMouseButton
 	if mb_event and mb_event.button_index == MOUSE_BUTTON_LEFT:
 		_resizing = not _resizing
-		_mouse_offset = get_local_mouse_position()
-		_window_position = _window.position
-		_window_size = _window.size
+		_mouse_start_pos = get_local_mouse_position()
+		_window_start_pos = _window.position
+		_window_start_size = _window.size
 
