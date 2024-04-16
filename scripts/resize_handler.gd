46921374d04877f3894aecb4ca01d3c8386cabe3
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-16 16:37:57 +0100

    Resize Handler: Replaced get_window and get_tree with global window and tree

--- scripts/resize_handler.gd
+++ scripts/resize_handler.gd
@@ -7,8 +7,6 @@ var _mouse_start_pos: Vector2
 var _window_start_pos: Vector2i
 var _window_start_size: Vector2i
 
-@onready var _window: Window = get_window()
-
 
 func _ready() -> void:
 	set_process(false)
@@ -16,33 +14,34 @@ func _ready() -> void:
 	gui_input.connect(_on_gui_input)
 
 	_distance_to_edge = Vector2i(
-		int(global_position.x) if global_position.x < _window.size.x / 2.0
-			else _window.size.x - int(global_position.x),
-		int(global_position.y) if (global_position.y < _window.size.y / 2.0)
-			else _window.size.y - int(global_position.y)
+		int(global_position.x) if global_position.x < Global.window.size.x / 2.0
+			else Global.window.size.x - int(global_position.x),
+		int(global_position.y) if (global_position.y < Global.window.size.y / 2.0)
+			else Global.window.size.y - int(global_position.y)
 	)
 
 
 func _process(_delta: float) -> void:
 	var is_diag := mouse_default_cursor_shape == CURSOR_BDIAGSIZE or mouse_default_cursor_shape == CURSOR_FDIAGSIZE
 	var mouse_delta: Vector2i = get_global_mouse_position() - _mouse_start_pos
+	var window := Global.window
 	if mouse_default_cursor_shape == CURSOR_HSIZE or is_diag:
-		if global_position.x < _window.size.x / 2.0:
-			if (_window.size.x != _window.max_size.x or mouse_delta.x > 0)\
-					and (_window.size.x != _window.min_size.x or mouse_delta.x < 0):
-				_window.position.x += mouse_delta.x - _distance_to_edge.x
-				_window.size.x = _window_start_size.x + _window_start_pos.x - _window.position.x
+		if global_position.x < window.size.x / 2.0:
+			if (window.size.x != window.max_size.x or mouse_delta.x > 0)\
+					and (window.size.x != window.min_size.x or mouse_delta.x < 0):
+				window.position.x += mouse_delta.x - _distance_to_edge.x
+				window.size.x = _window_start_size.x + _window_start_pos.x - window.position.x
 		else:
-			_window.size.x = mouse_delta.x + _distance_to_edge.x
+			window.size.x = mouse_delta.x + _distance_to_edge.x
 	
 	if mouse_default_cursor_shape == CURSOR_VSIZE or is_diag:
-		if global_position.y < _window.size.y / 2.0:
-			if (_window.size.y != _window.max_size.y or mouse_delta.y > 0)\
-					and (_window.size.y != _window.min_size.y or mouse_delta.y < 0):
-				_window.position.y += mouse_delta.y - _distance_to_edge.y
-				_window.size.y = _window_start_size.y + _window_start_pos.y - _window.position.y
+		if global_position.y < window.size.y / 2.0:
+			if (window.size.y != window.max_size.y or mouse_delta.y > 0)\
+					and (window.size.y != window.min_size.y or mouse_delta.y < 0):
+				window.size.y = _window_start_size.y + _window_start_pos.y - 	window.position.y
+				window.position.y += mouse_delta.y - _distance_to_edge.y
 		else:
-			_window.size.y = mouse_delta.y + _distance_to_edge.y
+			window.size.y = mouse_delta.y + _distance_to_edge.y
 
 
 
@@ -51,5 +50,5 @@ func _on_gui_input(event: InputEvent) -> void:
 	if mb_event and mb_event.button_index == MOUSE_BUTTON_LEFT:
 		set_process(not is_processing())
 		_mouse_start_pos = get_local_mouse_position()
-		_window_start_pos = _window.position
-		_window_start_size = _window.size
+		_window_start_pos = Global.window.position
+		_window_start_size = Global.window.size
