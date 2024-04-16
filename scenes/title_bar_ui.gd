fb2fb6993bb3fb75d3e20e2d22beb41f77fdbf98
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-16 16:18:55 +0100

    Title bar: Replaced get_window and get_tree with global window and tree

--- scenes/title_bar_ui.gd
+++ scenes/title_bar_ui.gd
@@ -29,8 +29,6 @@ var _previous_window_size: Vector2i
 var _previous_window_pinned_size: Vector2i
 var _previous_window_position: Vector2i
 
-@onready var _window: Window = get_window()
-
 
 func _enter_tree() -> void:
 	add_to_group(Main.SAVEABLE)
@@ -45,7 +43,7 @@ func _ready() -> void:
 	_b_pin.toggled.connect(_toggle_pin_window)
 	_b_minimise.pressed.connect(_minimise_window)
 
-	_window.size_changed.connect(_window_size_changed)
+	Global.window.size_changed.connect(_window_size_changed)
 
 	_l_title.text = ProjectSettings.get_setting("application/config/name")
 
@@ -58,35 +56,35 @@ func _ready() -> void:
 
 	await get_tree().process_frame
 	if _previous_window_pinned_size == Vector2i.ZERO:
-		_previous_window_pinned_size = _window.min_size
+		_previous_window_pinned_size = Global.window.min_size
 
 
 func _process(_delta: float) -> void:
-	_window.position += Vector2i(get_global_mouse_position() - _start_drag_pos)
+	Global.window.position += Vector2i(get_global_mouse_position() - _start_drag_pos)
 
 
 func save(save_data: Dictionary) -> void:
 	if _b_pin.button_pressed:
 		save_data[WINDOW_SIZE] = var_to_str(_previous_window_size)
-		save_data[WINDOW_PINNED_SIZE] = var_to_str(_window.size)
+		save_data[WINDOW_PINNED_SIZE] = var_to_str(Global.window.size)
 		save_data[WINDOW_POSITION] = var_to_str(_previous_window_position)
 	else:
-		save_data[WINDOW_SIZE] = var_to_str(_window.size)
+		save_data[WINDOW_SIZE] = var_to_str(Global.window.size)
 		save_data[WINDOW_PINNED_SIZE] = var_to_str(_previous_window_pinned_size)
-		save_data[WINDOW_POSITION] = var_to_str(_window.position)
+		save_data[WINDOW_POSITION] = var_to_str(Global.window.position)
 
 
 func load(save_data: Dictionary) -> void:
 	if save_data.has(WINDOW_SIZE):
 		_previous_window_size = str_to_var(save_data[WINDOW_SIZE])
-		_window.size = _previous_window_size
+		Global.window.size = _previous_window_size
 
 	_previous_window_pinned_size = str_to_var(save_data[WINDOW_PINNED_SIZE])\
-		if save_data.has(WINDOW_PINNED_SIZE) else _window.min_size
+		if save_data.has(WINDOW_PINNED_SIZE) else Global.window.min_size
 	
 	if save_data.has(WINDOW_POSITION):
 		_previous_window_position = str_to_var(save_data[WINDOW_POSITION])
-		_window.position = _previous_window_position
+		Global.window.position = _previous_window_position
 
 
 func _on_gui_input(event: InputEvent) -> void:
@@ -107,35 +105,35 @@ func _toggle_pin_window(pinning: bool) -> void:
 
 		_set_minimise_corner_radius(_b_close.get_theme_stylebox(HOVER).corner_radius_top_right)
 
-		_previous_window_position = _window.position
+		_previous_window_position = Global.window.position
 
-		_previous_window_size = _window.size
-		_window.size = _previous_window_pinned_size
+		_previous_window_size = Global.window.size
+		Global.window.size = _previous_window_pinned_size
 
-		var win_id := _window.current_screen
+		var win_id := Global.window.current_screen
 		var right := DisplayServer.screen_get_position(win_id).x\
 				+ DisplayServer.screen_get_size(win_id).x\
-				- _window.size.x\
+				- Global.window.size.x\
 				+ _window_margin_when_pinning.x
 		
-		_window.position = Vector2i(right, _window_margin_when_pinning.y)
+		Global.window.position = Vector2i(right, _window_margin_when_pinning.y)
 	else:
 		_b_pin.icon = _sprite_pin
 		_b_pin.set_tip_name("pin")
 
 		_set_minimise_corner_radius(_b_minimise.get_theme_stylebox(HOVER).corner_radius_top_left)
 
-		_previous_window_pinned_size = _window.size
-		_window.size = _previous_window_size
-
-		_window.position = _previous_window_position
+		_previous_window_pinned_size = Global.window.size
+		Global.window.size = _previous_window_size
+		
+		Global.window.position = _previous_window_position
 	
 	_b_close.visible = not pinning
-	_window.always_on_top = pinning
+	Global.window.always_on_top = pinning
 
 
 func _minimise_window() -> void:
-	_window.mode = Window.MODE_MINIMIZED
+	Global.window.mode = Window.MODE_MINIMIZED
 
 
 func _window_size_changed() -> void:
