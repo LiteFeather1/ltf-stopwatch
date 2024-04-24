964aeac4859de3859eee6da47b4f0baf438b1fca
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-04-24 17:09:15 +0100

    feat: added new inputs for deleting entries
    - Added undo and redo deleted pause entry
    - Renamed restore_last_elasped_time to restore_last_time_state

--- main.gd
+++ main.gd
@@ -47,7 +47,7 @@ func _ready() -> void:
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("restore_last_elapsed_time"):
+	if event.is_action_pressed("restore_last_time_state"):
 		_stopwatch_ui.restore_last_time_state()
 
 
