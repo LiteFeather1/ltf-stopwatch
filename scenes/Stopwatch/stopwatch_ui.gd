1f75b700ea901ccee2ac6e5c6417eeca5f808e76
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-05-06 12:14:28 +0100

    refactor: slight change on how heading is build
    - We resize to the maximum possible size and use the indexes to place each element correctly

--- scenes/Stopwatch/stopwatch_ui.gd
+++ scenes/Stopwatch/stopwatch_ui.gd
@@ -356,17 +356,18 @@ func _build_heading(
 	elapsed_time_template: String,
 	pause_span_template: String = "",
 ) -> PackedStringArray:
-	var heading := PackedStringArray([
-		pause_template % PAUSES,
-		pause_time_template % PAUSE_TIME,
-		resume_time_template % RESUME_TIME
-	])
+	var heading := PackedStringArray()
+	heading.resize(6)
+
+	heading[0] = pause_template % PAUSES
+	heading[2] = pause_time_template % PAUSE_TIME
+	heading[3] = resume_time_template % RESUME_TIME
 
 	if _copy_menu_options_mask & CopyMenuFlags.ELAPSED_TIMES:
-		heading.insert(1, elapsed_time_template % ELAPSED_TIME)
+		heading[1] = elapsed_time_template % ELAPSED_TIME
 
 	if _copy_menu_options_mask & CopyMenuFlags.PAUSE_SPANS:
-		heading.append(pause_span_template % PAUSE_SPAN)
+		heading[4] = pause_span_template % PAUSE_SPAN
 
 	return heading
 
