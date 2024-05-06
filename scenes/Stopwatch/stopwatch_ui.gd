d256c3c2dbc2413b4b0bc2ab67d07b9cf6a24e14
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-05-06 14:16:39 +0100

    fix: copy menu markdown correctly formats it's heading and entries

--- scenes/Stopwatch/stopwatch_ui.gd
+++ scenes/Stopwatch/stopwatch_ui.gd
@@ -405,7 +405,7 @@ func _copy_menu_csv(_index: int) -> void:
 
 
 func _copy_menu_markdown(_index: int) -> void:
-	var heading := _build_heading("|%s", "|%s", "|%s|", "|%s", "%s|")
+	var heading := _build_heading("|%s", "|%s", "|%s|", "|%s", "%s|", "%s|")
 	var heading_size := heading.size()
 	heading.resize(heading_size * 2 + 1)
 	heading[heading_size] = "\n"
@@ -414,7 +414,7 @@ func _copy_menu_markdown(_index: int) -> void:
 		heading[i + heading_size + 2] = ":-:|"
 
 	_copy_menu_tray_entries("MD Table", PackedStringArray(["".join(heading)]),
-		"|%s%s|%s|%s|%s", "|%s", "%s|"
+		"|%s%s|%s|%s|%s", "|%s", "%s|", "%s|"
 	)
 
 
