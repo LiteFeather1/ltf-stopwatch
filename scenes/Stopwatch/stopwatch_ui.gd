908c180aae4c3e2166a9c76c2365c531d9740ebb
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-05-07 14:19:43 +0100

    feat: added number sign to copy menu entries

--- scenes/Stopwatch/stopwatch_ui.gd
+++ scenes/Stopwatch/stopwatch_ui.gd
@@ -365,7 +365,7 @@ func _copy_menu_tray_entries(
 
 func _copy_menu_simple(_index: int) -> void:
 	_copy_menu_tray_entries("Simple", PackedStringArray(),
-		"%s  %s%s  %s%s%s", "%s  ", " %s", "  %s"
+		"#%s  %s%s  %s%s%s", "%s  ", " %s", "  %s"
 	)
 
 
@@ -405,7 +405,7 @@ func _copy_menu_long(_index: int) -> void:
 		"%s  |", "  %s  |", "  %s", "  %s  |", "  |  %s", "  |  %s"
 	))])
 	_copy_menu_tray_entries("Long", entries_text,
-		"%s            %s|     %s     |     %s%s%s", "|      %s       ", "        |    %s", (
+		"#%s          %s|     %s     |     %s%s%s", "|      %s       ", "        |    %s", (
 			"     |  %s" if _copy_menu_options_mask & CopyMenuFlags.PAUSE_SPANS != 0 else "        |  %s"
 		)
 	)
@@ -415,7 +415,7 @@ func _copy_menu_csv(_index: int) -> void:
 	var entries_text := PackedStringArray(["".join(
 		_build_heading("%s,", "%s,", "%s", "%s,", ",%s", ",%s"
 	))])
-	_copy_menu_tray_entries("CSV", entries_text, "%s,%s%s,%s%s%s", "%s,", ",%s", ",%s")
+	_copy_menu_tray_entries("CSV", entries_text, "#%s,%s%s,%s%s%s", "%s,", ",%s", ",%s")
 
 
 func _copy_menu_markdown(_index: int) -> void:
@@ -431,7 +431,7 @@ func _copy_menu_markdown(_index: int) -> void:
 		":-|" if (_copy_menu_options_mask & CopyMenuFlags.LONGEST_SHORTEST != 0) else ":-:|"
 
 	_copy_menu_tray_entries("MD Table", PackedStringArray(["".join(heading)]),
-		"|%s%s|%s|%s|%s%s", "|%s", "%s|", "%s|"
+		"|#%s%s|%s|%s|%s%s", "|%s", "%s|", "%s|"
 	)
 
 
