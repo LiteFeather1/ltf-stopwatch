5bf040ccc2ca41a7086cc25e2ef408931e3767a1
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-05-14 16:14:40 +0100

    refactor: separated the resize vertical and horizontal logic into methods

--- scripts/resize_handler.gd
+++ scripts/resize_handler.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 		global_pos.x if global_position.x < GLOBAL.window.size.x * .5
 			else GLOBAL.window.size.x - global_pos.x,
 		global_pos.y if (global_position.y < GLOBAL.window.size.y * .5)
-			else GLOBAL.window.size.y - global_pos.y
+			else GLOBAL.window.size.y - global_pos.y,
 	)
 
 
@@ -25,39 +25,49 @@ func _input(event: InputEvent) -> void:
 	if not m_event:
 		return
 
-	var is_diag := (
+	var mouse_delta := Vector2i(m_event.position) - _mouse_start_pos
+	if (
 		mouse_default_cursor_shape == CURSOR_BDIAGSIZE
 		or mouse_default_cursor_shape == CURSOR_FDIAGSIZE
-	)
-	var mouse_delta := Vector2i(m_event.position) - _mouse_start_pos
-	var win := GLOBAL.window
-	if mouse_default_cursor_shape == CURSOR_HSIZE or is_diag:
-		if global_position.x < win.size.x / 2.0:
-			if (
-				(win.size.x != win.max_size.x or mouse_delta.x > 0)
-				and (win.size.x != win.min_size.x or mouse_delta.x < 0)
-			):
-				win.position.x += mouse_delta.x - _distance_to_edge.x
-				win.size.x = _window_start_size.x + _window_start_pos.x - win.position.x
-		else:
-			win.size.x = mouse_delta.x + _distance_to_edge.x
-	
-	if mouse_default_cursor_shape == CURSOR_VSIZE or is_diag:
-		if global_position.y < win.size.y / 2.0:
-			if (
-				(win.size.y != win.max_size.y or mouse_delta.y > 0)
-				and (win.size.y != win.min_size.y or mouse_delta.y < 0)
-			):
-				win.size.y = _window_start_size.y + _window_start_pos.y - win.position.y
-				win.position.y += mouse_delta.y - _distance_to_edge.y
-		else:
-			win.size.y = mouse_delta.y + _distance_to_edge.y
+	):
+		_horizontal_resize(mouse_delta.x)
+		_vertical_resize(mouse_delta.y)
+	elif mouse_default_cursor_shape == CURSOR_HSIZE:
+		_horizontal_resize(mouse_delta.x)
+	elif mouse_default_cursor_shape == CURSOR_VSIZE:
+		_vertical_resize(mouse_delta.y)
 
 
 func _gui_input(event: InputEvent) -> void:
 	var mb_event := event as InputEventMouseButton
 	if mb_event and mb_event.button_index == MOUSE_BUTTON_LEFT:
-		set_process_input(not is_processing_input())
 		_mouse_start_pos = mb_event.position
 		_window_start_pos = GLOBAL.window.position
 		_window_start_size = GLOBAL.window.size
+		set_process_input(not is_processing_input())
+
+
+func _horizontal_resize(x_delta: int) -> void:
+	var win := GLOBAL.window
+	if global_position.x < win.size.x * .5:
+		if (
+			(win.size.x != win.max_size.x or x_delta > 0)
+			and (win.size.x != win.min_size.x or x_delta < 0)
+		):
+			win.position.x += x_delta - _distance_to_edge.x
+			win.size.x = _window_start_size.x + _window_start_pos.x - win.position.x
+	else:
+		win.size.x = x_delta + _distance_to_edge.x
+
+
+func _vertical_resize(y_delta: int) -> void:
+	var win := GLOBAL.window
+	if global_position.y < win.size.y * .5:
+		if (
+			(win.size.y != win.max_size.y or y_delta > 0)
+			and (win.size.y != win.min_size.y or y_delta < 0)
+		):
+			win.position.y += y_delta - _distance_to_edge.y
+			win.size.y = _window_start_pos.y + _window_start_size.y - win.position.y
+	else:
+		win.size.y = y_delta + _distance_to_edge.y
