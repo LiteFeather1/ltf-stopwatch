da1148b21db7dac3be0dd6d3d693e16513be1bd1
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-05-24 13:57:09 +0100

    feat: on window pinned we now darken the window and lighten on unpin

--- main.gd
+++ main.gd
@@ -8,15 +8,21 @@ const VERSION := &"version"
 const SAVEABLE := &"saveable"
 
 @export_category("Window")
-@export var _min_window_size := Vector2i(192, 192)
-@export var _max_window_size := Vector2i(512, 512)
+@export var _min_window_size: Vector2i = Vector2i(192, 192)
+@export var _max_window_size: Vector2i = Vector2i(512, 512)
+
+@export_category("Colours")
+@export var _pinned_colour: Color = Color("#181818")
 
 @export_category("Nodes")
 @export var _stopwatch_ui: StopwatchUI
 @export var _title_bar_ui: TitleBarUI
 
+var _normal_colour: Color
+
 
 func _ready() -> void:
+	_title_bar_ui.pin_toggled.connect(_on_title_bar_ui_pin_toggled)
 	_title_bar_ui.close_pressed.connect(_quit_app)
 	
 	GLOBAL.window.close_requested.connect(_quit_app)
@@ -29,6 +35,8 @@ func _ready() -> void:
 
 	GLOBAL.window.min_size = _min_window_size
 	GLOBAL.window.max_size = _max_window_size
+
+	_normal_colour = get_theme_stylebox("panel").bg_color
 
 	if not FileAccess.file_exists(SAVE_PATH):
 		print("No file at %s" % SAVE_PATH)
@@ -60,6 +68,10 @@ func _unhandled_key_input(event: InputEvent) -> void:
 		_stopwatch_ui.redo_deleted_stopwatch_entry_ui()
 	elif event.is_action_pressed("undo_deleted_pause_entry"):
 		_stopwatch_ui.undo_deleted_stopwatch_entry_ui()
+
+
+func _on_title_bar_ui_pin_toggled(pinning: bool) -> void:
+	get_theme_stylebox("panel").bg_color = _pinned_colour if pinning else _normal_colour
 
 
 func _quit_app() -> void:
