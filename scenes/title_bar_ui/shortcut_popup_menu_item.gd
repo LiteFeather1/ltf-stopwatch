abcfa05c3cc8fcd99524a855e527cb87abca80df
LiteFeather1 <102820899+LiteFeather1@users.noreply.github.com>
2024-06-05 14:16:32 +0100

    feat: Shortcut Popup Menu Item subclass

---
+++ scenes/title_bar_ui/shortcut_popup_menu_item.gd
@@ -0,0 +1,8 @@
+class_name ShortcutPopupMenuItem extends PopupMenuItem
+
+
+@export var _shortcut: Shortcut
+
+
+func add_to_popup_menu(menu: PopupMenu, id: int) -> void:
+	menu.add_icon_shortcut(_icon, _shortcut, id)
